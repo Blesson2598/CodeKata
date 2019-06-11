123b5db82955025e26155318e8152ae9b42db255
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-06-11 09:36:02 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 #include <stdlib.h>
- 
+#include<string.h>
+#include<ctype.h>
 int main()
 {
-    long long int num;
-    scanf("%lld",&num);
+    char a[100];
+    scanf("%s",&a);
+    int l=strlen(a);
+    for(int i=0;i<l;i++)
+    {
+    	if(isalpha(a[i]))
+    	{
+    		printf("Invalid");
+    		return 0;
+    	}
+    }
+    int num=atoi(a);
     if(num<0)
     printf("Negative");
     if(num==0)

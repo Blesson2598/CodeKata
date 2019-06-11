b4ad9bccab30cd00eee9f3555e675a6cb38126f5
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-06-11 10:47:07 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+ char a[10]={'H','e','l','l','o','\0'};
+ int n;
+ scanf("%d",&n);
+ for(int i=0;i<n-1;i++,printf("\n"))
+	printf("%s",a);
+	if(n!=0)
+	printf("%s",a);
+	return 0;
+}

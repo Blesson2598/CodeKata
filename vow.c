6108390b127cb692f527a6d3b8cca2871792965c
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-06-11 10:15:32 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,10 @@
+s=input();
+v="aeiouAeiou";c="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+l=len(s)
+for i in range(l):
+	if s in v:
+		print("Vowel");
+	if s not in v and s in c:
+		print("Consonant");
+	if s not in v and c:
+		print("invalid");

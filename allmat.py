a5fa48faad23e5898836c5191d62f70a628a1a52
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-18 14:53:24 +0530

    Create allmat.py
    r,c=input().split(" ")
    r=int(r);c=int(c)
    # r=int(size[0]);c=int(size[1])
    mat=[]
    for i in range(r):
    	s=input().rstrip()
    	arr=list(map(int,s.split(" ")))
    	mat.append(arr)
    fin=[]	
    for i in range(0,1):
    	for j in range(c):
    		for k in range(1,r):
    			f=0
    			if mat[i][j] in mat[k]:
    				continue
    			else:
    				f=1
    				break
    		if f==0:
    			fin.append(mat[i][j])
    fin.sort()
    print(*fin,sep=" ")

---
+++ allmat.py
@@ -0,0 +1,22 @@
+r,c=input().split(" ")
+r=int(r);c=int(c)
+# r=int(size[0]);c=int(size[1])
+mat=[]
+for i in range(r):
+	s=input().rstrip()
+	arr=list(map(int,s.split(" ")))
+	mat.append(arr)
+fin=[]	
+for i in range(0,1):
+	for j in range(c):
+		for k in range(1,r):
+			f=0
+			if mat[i][j] in mat[k]:
+				continue
+			else:
+				f=1
+				break
+		if f==0:
+			fin.append(mat[i][j])
+fin.sort()
+print(*fin,sep=" ")

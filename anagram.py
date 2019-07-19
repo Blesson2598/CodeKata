c739d07a682dc8136b554483395fa7b695e778a6
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-19 05:31:44 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,7 +1,9 @@
 s1="dhoni"
 s2=input()
-val=list(set(s1)-set(s2))
-if len(val)==0:
+val1=list(set(s1)-set(s2))
+val2=list(set(s2)-set(s1))
+val=len(val1)+len(val2)
+if val==0:
 	print("yes")
 else:
 	print("no")

b1617167e913598668767643ad9728da2350643c
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-20 21:05:51 +0530

    Update allbin.py

--- allbin.py
+++ allbin.py
@@ -1,5 +1,6 @@
 n=int(input())
 limit=pow(2,n)
+final=[]
 for i in range(limit):
 	val=bin(i)
 	val=val[2:]
@@ -9,4 +10,13 @@ for i in range(limit):
 	li=[]
 	li.append(s)
 	li.append(val)
-	print(*li,sep="")
+	final.append(s+val)
+order=[]	
+order.append(final[0])
+c=1
+while c<=n:
+	for i in range(limit):
+		if final[i].count('1')==c:
+			order.append(final[i])
+	c+=1		
+print(*order,sep="\r")		

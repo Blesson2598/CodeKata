dee19c9b476ddd163adfeef63fd774564d2b75a5
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-20 20:41:01 +0530

    Update max_sum.py

--- max_sum.py
+++ max_sum.py
@@ -1,9 +1,23 @@
 size=int(input())
 arr=input()
 arr=list(map(int,arr.split(" ")))
-sum1=0;c=size//2
-while c>0:
-	c-=1
-	sum1+=max(arr)
-	arr.remove(max(arr))
-print(sum1)	
+sum1=0;c=2;all_sum=[]
+j=0
+while j<size:
+	sum1=0
+	for i in range(0,size,c):
+		sum1+=arr[i]
+	all_sum.append(sum1)
+	c+=1
+	j+=1
+j=0
+c=2
+while j<size:
+	sum1=0
+	for i in range(1,size,c):
+		sum1+=arr[i]
+	all_sum.append(sum1)
+	c+=1
+	j+=1
+print(max(all_sum))	
+	

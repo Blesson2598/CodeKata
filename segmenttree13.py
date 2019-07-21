ca867c155b81d78a7e5ac8f4170c3d00af2fa561
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-21 23:04:48 +0530

    Create segmenttree13.py

---
+++ segmenttree13.py
@@ -0,0 +1,9 @@
+row,col=input().split()
+col=int(col)
+values=input()
+values=list(map(int,values.split(" ")))
+for i in range(col):
+	nodes=input()
+	nodes=list(map(int,nodes.split(" ")))
+	temp=values[nodes[0]-1:nodes[1]]
+	print(min(temp))

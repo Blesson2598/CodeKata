7ec01f5cca1b0dc9e1988c7ad887b850f82ee732
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-21 23:40:55 +0530

    Update and rename segmenttree13.py to segmenttree13.c

---
+++ segmenttree13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int size,vals;
+	scanf("%d %d",&size,&vals);
+	int arr[size],iterator1,iterator2;
+	for(iterator1=0;iterator1<size;iterator1++)
+		scanf("%d",&arr[iterator1]);
+	int start,end;
+	for(iterator1=0;iterator1<vals;iterator1++)
+	{
+	scanf("%d %d",&start,&end);
+	int min=10000;
+	for(iterator2=start-1;iterator2<end;iterator2++)
+	{
+		if(min>arr[iterator2])
+		min=arr[iterator2];
+	}
+	printf("%d\n",min);
+	}
+	return 0;
+}
--- segmenttree13.py
+++
@@ -1,9 +0,0 @@
-row,col=input().split()
-col=int(col)
-values=input().rstrip()
-values=list(map(str,values.split(" ")))
-for i in range(col):
-	nodes=input()
-	nodes=list(map(int,nodes.split(" ")))
-	temp=values[nodes[0]-1:nodes[1]]
-	print(min(temp))

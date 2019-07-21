db5571beab73316bf32c515cacb053bbf0ace374
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-21 23:26:15 +0530

    Create segxor.py

---
+++ segxor.py
@@ -0,0 +1,13 @@
+row,col=input().split()
+col=int(col)
+values=input().rstrip()
+values=list(map(int,values.split(" ")))
+for i in range(col):
+	nodes=input()
+	nodes=list(map(int,nodes.split(" ")))
+	temp=values[nodes[0]-1:nodes[1]]
+	x=0
+	l=len(temp)
+	for j in range(l):
+		x^=temp[j]
+	print(x,end="\r")

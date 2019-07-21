b17caa5ba061a1bca43b0ced0d71b0c2ae2a7693
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-21 23:09:18 +0530

    Update segmenttree13.py

--- segmenttree13.py
+++ segmenttree13.py
@@ -1,7 +1,7 @@
 row,col=input().split()
 col=int(col)
 values=input().rstrip()
-values=list(map(int,values.split(" ")))
+values=list(map(str,values.split(" ")))
 for i in range(col):
 	nodes=input()
 	nodes=list(map(int,nodes.split(" ")))

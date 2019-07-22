98433d70a6241f80876b82b3c2344dc1c84ce4a8
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-23 05:22:42 +0530

    Update localmaxmin.py

--- localmaxmin.py
+++ localmaxmin.py
@@ -4,16 +4,7 @@ arr=list(map(int,arr.split(" ")))
 count=0;m=0
 temp=arr[:]
 temp.sort()
-if temp==arr:
-	print(m)
-	exit()
-for i in range(1,size-1):
-	count=0
-	if arr[i]>arr[i-1]:
+for i in range(size):
+	if temp[i]!=arr[i]:
 		count+=1
-	if arr[i]<arr[i+1]:
-		count+=1
-	if count>m:
-		m=count
-print(m)		
-		
+print(count)		

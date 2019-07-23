9ba550df561b4d3e07eb5076cd6dc2da1ee8596d
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-23 22:31:06 +0530

    Create kpairs.py

---
+++ kpairs.py
@@ -0,0 +1,16 @@
+size,val=input().split()
+size=int(size);val=int(val)
+arr=input().rstrip()
+arr=list(map(int,arr.split(" ")))
+li=[]
+for i in range(size):
+	if arr[i]<val:
+		li.append(arr[i])
+l=len(li);c=0
+m=max(li)
+while c<l-1:
+	if (li[c]+m)==val:
+		print("yes")
+		exit()
+	c+=1	
+print("no")		

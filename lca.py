0d6b74d1c213743fa16e434b9a39f6ff7ce82c85
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-26 20:27:02 +0530

    Update lca.py

--- lca.py
+++ lca.py
@@ -3,6 +3,11 @@ arr=list(map(int,input().split()))
 val1,val2=map(int,input().split())
 ind1=arr.index(val1)
 ind2=arr.index(val2)
+temp=arr[:]
+temp.sort()
+if temp==arr:
+	print(min(val1,val2))
+	exit()
 if abs(ind1-ind2)==1:
 	print(min(val1,val2))
 else:

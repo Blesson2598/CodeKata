3f985408304fe1b3d5176626d737bda929c97973
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-26 19:43:41 +0530

    Create binprime.py

---
+++ binprime.py
@@ -0,0 +1,20 @@
+import math
+def isprime(c):
+	sq=int(math.sqrt(c))
+	for i in range(2,sq+1):
+		if c%i==0:
+			return 0
+	return 1		
+		
+	
+	
+start,end=list(map(int,input().split()));count=0
+for i in range(start,end):
+	temp=bin(i)
+	temp=temp[2:]
+	c=temp.count('1')
+	f=isprime(c)
+	if f==1 and i!=1:
+		count+=1
+print(count)		
+		

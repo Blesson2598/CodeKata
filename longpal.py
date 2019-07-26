2e4fd011f2eb559dfcb7f4ca84f98c208529817d
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-07-27 05:24:02 +0530

    Create longpal.py

---
+++ longpal.py
@@ -0,0 +1,16 @@
+s=input();l=len(s)
+subs=[];c=1
+while c<l:
+	for i in range(0,l):
+		temp=s[i:i+c]
+		subs.append(temp)
+	c+=1
+l=len(subs)
+pal=['1']
+for i in range(l):
+	temp=subs[i][:]
+	temp=temp[::-1]
+	if subs[i]!=temp and len(temp)>len(pal[0]):
+		pal.remove(pal[0])
+		pal.append(subs[i])
+print(*pal,sep="")	

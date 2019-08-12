a8b2717f3edee403a9b94aa00081bd88c79550b2
Blesson2598 <46119418+Blesson2598@users.noreply.github.com>
2019-08-12 17:55:16 +0530

    Update balance.py

--- balance.py
+++ balance.py
@@ -2,11 +2,19 @@ left,right=map(str,input().split("|"))
 append=input()
 if len(left)==len(right):
 	print("Impossible")
-elif abs(len(left)-len(right))==1:
+if len(left)==0:
+	if len(append)==len(right):
+		print(append,'|',right,sep="")
+	else:
+		print("Impossible")
+if len(right)==0:
+	if len(append)==len(left):
+		print(append,'|',right,sep="")
+	else:
+		print("Impossible")		
+if abs(len(left)-len(right))==1:
 	if len(left)>len(right):
 		right+=append
 	else:
 		left+=append
 	print(left,'|',right,sep="")
-else:
-	print("Impossible")
